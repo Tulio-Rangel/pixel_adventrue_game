2a8b6c4ec126cb4d3c7261a198d42d6292b5ea3d
Tulio-Rangel <tjrn20@hotmail.com>
2025-06-12 10:38:45 -0500

    Integrar el juego PixelAdventure en la aplicación principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:pixel_adventure_game/pixel_adventure.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  PixelAdventure game = PixelAdventure();
+  runApp(GameWidget(game: game));
 }
