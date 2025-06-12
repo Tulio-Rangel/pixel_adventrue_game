dc197f94c1db8213300fd1dab70099773e87ebda
Tulio-Rangel <tjrn20@hotmail.com>
2025-06-12 10:42:11 -0500

    Configurar la inicialización de Flutter y establecer el modo de pantalla completa y paisaje

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:pixel_adventure_game/pixel_adventure.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensures Flutter is initialized before running the app
+  Flame.device.fullScreen(); // Sets the app to full screen mode
+  Flame.device.setLandscape(); // Sets the app to landscape mode
+
   PixelAdventure game = PixelAdventure();
   runApp(GameWidget(game: game));
 }
