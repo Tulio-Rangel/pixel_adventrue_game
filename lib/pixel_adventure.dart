14491645b1609721ead357b018fcdebc3f095ec7
Tulio-Rangel <tjrn20@hotmail.com>
2025-06-13 15:37:58 -0500

    Agregar la clase Level y cargar el mapa Tiled en el juego Pixel Adventure

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,3 +1,28 @@
-import 'package:flame/game.dart';
+import 'dart:async';
+import 'dart:ui';
 
-class PixelAdventure extends FlameGame {}
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:pixel_adventure_game/levels/level.dart';
+
+class PixelAdventure extends FlameGame {
+  @override
+  Color backgroundColor() => const Color(0xFF211F30); // Set the background color of the game
+  late final CameraComponent cam;
+
+  final world = Level(); // Create an instance of Level
+
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(
+      world: world, // Use the world from the game
+      width: 640,
+      height: 360,
+    ); // Create a camera with a fixed resolution
+    cam.viewfinder.anchor =
+        Anchor.topLeft; // Set the camera's anchor to the top-left corner
+
+    addAll([cam, world]); // Add the camera and world to the game
+    return super.onLoad();
+  }
+}
