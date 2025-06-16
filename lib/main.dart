73e157cb1c04b5b1abcbf24c0b77c9370882a336
Tulio-Rangel <tjrn20@hotmail.com>
2025-06-16 10:35:37 -0500

    Actualizar la función main para inicializar correctamente el modo de pantalla completa y paisaje de forma asíncrona

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pixel_adventure_game/pixel_adventure.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensures Flutter is initialized before running the app
-  Flame.device.fullScreen(); // Sets the app to full screen mode
-  Flame.device.setLandscape(); // Sets the app to landscape mode
+  await Flame.device.fullScreen(); // Sets the app to full screen mode
+  await Flame.device.setLandscape(); // Sets the app to landscape mode
 
   PixelAdventure game = PixelAdventure();
   runApp(GameWidget(game: kDebugMode ? PixelAdventure() : game));
