79af51a0937f5e8d4564d0fdd34b2512b17cfdac
Tulio-Rangel <tjrn20@hotmail.com>
2025-06-16 11:23:59 -0500

    Agregar soporte para joystick y botones en la interfaz de usuario del juego

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -4,14 +4,17 @@ import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 import 'package:pixel_adventure_game/actors/player.dart';
 import 'package:pixel_adventure_game/levels/level.dart';
 
-class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents {
+class PixelAdventure extends FlameGame
+    with HasKeyboardHandlerComponents, DragCallbacks {
   @override
   Color backgroundColor() => const Color(0xFF211F30); // Set the background color of the game
   late final CameraComponent cam;
   Player player = Player(character: 'Mask Dude');
+  late JoystickComponent joystick;
 
   @override
   FutureOr<void> onLoad() async {
@@ -32,6 +35,31 @@ class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents {
         Anchor.topLeft; // Set the camera's anchor to the top-left corner
 
     addAll([cam, world]); // Add the camera and world to the game
+
+    addJoystick(); // Add a joystick for player control
     return super.onLoad();
+  }
+
+  void addJoystick() {
+    final joystick = JoystickComponent(
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Knob.png'), // Load the joystick knob sprite
+        ),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache(
+            'HUD/Joystick.png',
+          ), // Load the joystick background sprite
+        ),
+      ),
+      margin: const EdgeInsets.only(
+        left: 5,
+        bottom: 32,
+      ), // Set the margin for the joystick
+    ); // Create a joystick component with a directional joystick
+
+    add(joystick); // Add the joystick to the game
   }
 }
