356f075e62c88fa78913bd5cf659670d93381a9b
Tulio-Rangel <tjrn20@hotmail.com>
2025-06-16 10:24:08 -0500

    Agregar soporte para el manejo de teclado en la clase Player y actualizar la lógica de movimiento del jugador

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flutter/src/services/hardware_keyboard.dart';
+import 'package:flutter/src/services/keyboard_key.g.dart';
 import 'package:pixel_adventure_game/pixel_adventure.dart';
 
 enum PlayerState {
@@ -8,8 +10,16 @@ enum PlayerState {
   running, // Player is running
 }
 
+enum PlayerDirection {
+  left, // Player is facing left
+  right, // Player is facing right
+  up, // Player is facing up
+  down, // Player is facing down
+  none, // Player is not facing any direction
+}
+
 class Player extends SpriteAnimationGroupComponent
-    with HasGameRef<PixelAdventure> {
+    with HasGameRef<PixelAdventure>, KeyboardHandler {
   Player({position, required this.character}) : super(position: position) {
     // Initialize the player with a specific position and character
   }
@@ -19,10 +29,45 @@ class Player extends SpriteAnimationGroupComponent
   late final SpriteAnimation runningAnimation; // Animation for running state
   final double stepTime = 0.05; // Time between frames in seconds
 
+  PlayerDirection playerDirection =
+      PlayerDirection.none; // Current direction of the player
+  double moveSpeed = 100; // Movement speed of the player in pixels per second
+  Vector2 velocity = Vector2.zero(); // Current velocity of the player
+  bool isFacingRight = true; // Whether the player is facing right
+
   @override
   FutureOr<void> onLoad() {
     _loadAllAnimations();
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    _updatePlayerMovement(dt);
+    super.update(dt);
+  }
+
+  @override
+  bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    final isLeftKeyPress =
+        keysPressed.contains(LogicalKeyboardKey.arrowLeft) ||
+        keysPressed.contains(LogicalKeyboardKey.keyA);
+
+    final isRightKeyPress =
+        keysPressed.contains(LogicalKeyboardKey.arrowRight) ||
+        keysPressed.contains(LogicalKeyboardKey.keyD);
+
+    if (isLeftKeyPress && isRightKeyPress) {
+      playerDirection =
+          PlayerDirection.none; // No movement if both keys are pressed
+    } else if (isLeftKeyPress) {
+      playerDirection = PlayerDirection.left; // Move left
+    } else if (isRightKeyPress) {
+      playerDirection = PlayerDirection.right; // Move right
+    } else {
+      playerDirection = PlayerDirection.none; // No movement
+    }
+    return super.onKeyEvent(event, keysPressed);
   }
 
   void _loadAllAnimations() {
@@ -54,5 +99,38 @@ class Player extends SpriteAnimationGroupComponent
         loop: true, // Loop the animation
       ),
     );
+  }
+
+  void _updatePlayerMovement(double dt) {
+    double dirX = 0.0;
+    switch (PlayerDirection) {
+      case PlayerDirection.left:
+        if (isFacingRight) {
+          // Flip the sprite if changing direction from right to left
+          flipHorizontallyAroundCenter();
+          isFacingRight = false;
+        }
+        current = PlayerState.running; // Set the state to running when moving
+        dirX -= moveSpeed;
+        break;
+      case PlayerDirection.right:
+        if (!isFacingRight) {
+          // Flip the sprite if changing direction from left to right
+          flipHorizontallyAroundCenter();
+          isFacingRight = true;
+        }
+        current = PlayerState.running; // Set the state to running when moving
+        dirX += moveSpeed;
+        break;
+      case PlayerDirection.none:
+        current = PlayerState.idle; // Set the state to idle when not moving
+        break;
+      default:
+    }
+
+    velocity = Vector2(dirX, 0); // Update the velocity based on direction
+    position +=
+        velocity *
+        dt; // Update the player's position based on velocity and delta time
   }
 }
